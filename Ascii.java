053b7a0d27fb2f116c1752d3f73c098011b344b0
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-11-21 22:08:56 +0530

    Ascii.java
    ASCII - American Standard Code for Information Interchange. Write a program to get a number(ASCII) as input and print its equivalent character. You can display a character if you know ASCII code of that character. 
    
    Sample Input:
    
    103
    
    Sample Output:
    
    g

---
+++ Ascii.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ascii
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        char a = (char)c;
+        System.out.println(a);
+    }
+}
