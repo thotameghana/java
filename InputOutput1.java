53369acacee977c3bb115f32a26e62a252d1df6a
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-11-21 22:00:18 +0530

    InputOutput1.java
    Write a program to get an integer as input and print it. When the user enters an integer, it is stored in a variable test Integer. NOTE: Bold letters or words are Input and the rest are output statements.
    
    Sample Input:
    
     56
    
     Sample Output:
    
     56

---
+++ InputOutput1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class InputOutput1
+{
+    public static void main(String args[])
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        System.out.println(+a);
+        
+    }
+}
