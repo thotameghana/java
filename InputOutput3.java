172b7b4126f55e5e8768518121c5c947ab9ef3d3
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-11-21 22:03:37 +0530

    InputOutput3.java
    Write a program to get a character as input and print it. When the user enters a character, it is stored in the variable test Character. NOTE: Bold letters or words are Input and the rest are output statements.
    
    Sample Input:
    
    z
    
    Sample Output:
    
    z

---
+++ InputOutput3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class InputOutput3
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        System.out.println(a);
+    }
+}
