859b0b9d85925fb53a57ed85f6cdc2f4276f7242
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-11-21 22:07:17 +0530

    InputOutput5.java
    ASCII - American Standard Code for Information Interchange. Write a program to get a character as input and print it's ASCII value. When a character is entered, the character itself  stored.  And when we displayed that value using "int()"  type casting , the entered character is displayed as Ascii value. NOTE: Bold letters or words are Input and rest are output statements.
    
    Sample Input :
    
    g
    
    Sample Output:
    
    103

---
+++ InputOutput5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class InputOutput5
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char c=sc.next().charAt(0);
+        int a=c;
+        System.out.println(a);
+    }
+}
