2a3f1021f166eda9ca879f9c5bc136b127893478
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-11-29 10:14:56 +0530

    SumDiff.java
    Write a program to get 2 numbers from the user and calculate their sum and difference using '+' and '-' operators respectively. Print the corresponding sum and difference of the numbers as output in the console.
    
     Input format:
    
    First input: an integer 
    
    Second input: an integer 
    
    Output format:
    
    First output will be the sum of two integers
    
    Second output will be the difference of two integers
    
    Sample Input:
    
    55
    
    34
    
    Sample Output:
    
    89
    
    21

---
+++ SumDiff.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class SumDiff
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+    }
+}
