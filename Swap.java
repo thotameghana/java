a70d73a84f88fc4fabf5dbcd58dfdfae61ae58a0
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-11-29 10:16:58 +0530

    Swap.java
    Write a program to get 2 numbers from the user and swap their values without any loss of data. You can make use of an additional 3rdvariable for swapping. Print the corresponding swapped values of the two numbers as output in the console. 
    
    Input format: 
    
    First input: an integer 
    
    Second input: an integer 
    
    Output format: 
    
    The output will be integers(swapped values)
    
    Sample Input:
    
    20
    
    10
    
    Sample Output:
    
    10
    
    20

---
+++ Swap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Swap
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println(c-a);
+        System.out.println(c-b);
+        
+    }
+}
