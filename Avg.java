2d359d3011a9a21723fd204188127b98be2104ad
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-11-29 10:13:28 +0530

    Avg.java
    A teacher wants to compute the average of 5 students in her class. Write a program to help her to find the average. The average is the sum of all the numbers, then divided by the total numbers. 
    
    Input format: 
    
    First input: 1st student mark in float 
    
    Second input: 2nd student mark in float 
    
    Third input: 3rd student mark in float 
    
    Fourth input:4th student mark in float 
    
    Fifth input: 5th student mark in float 
    
    Output format: 
    
    The output value should be in float with 2 decimal places.
    
    Sample Input: 
    
    10
    
    20
    
    30
    
    40
    
    50 
    
    Sample Output:
    
     30.00

---
+++ Avg.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Avg
+{
+    public static void main(String args[])
+    {
+        float a,b,c,d,e;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        c=sc.nextFloat();
+        d=sc.nextFloat();
+        e=sc.nextFloat();
+        System.out.println((a+b+c+d+e)/5);
+        
+    }
+}
