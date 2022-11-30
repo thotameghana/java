83b78fa1a5c902c6bc03c9bc74c42afc16e7216b
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-11-30 14:39:59 +0530

    Create Football Stadium.java
    There was a large ground in center of the city which is rectangular in shape. The Corporation decides to build a Football stadium in the area for school and college students, But the area was used as a car parking zone. In order to protect the land from using as an unauthorized parking zone , the corporation wanted to protect the stadium by building a fence. In order to help the workers to build a fence, they planned to place a thick rope around the ground. They wanted to buy only the exact length of the rope that is needed. They also wanted to cover the entire ground with a carpet during rainy season. They wanted to buy only the exact quantity of carpet that is needed. They requested your help.
    Can you please help them by writing a program to find the exact length of the rope and the exact quantity of carpet that is required?
    
    Input format:
    Input consists of 2 integers.
    The first integer corresponds to the length of the ground
    The second integer corresponds to the breadth of the ground.
    Output Format:
    Output Consists of two integers.
    The first integer corresponds to the length.
    The second integer corresponds to the quantity of carpet required.
    
    Sample Input:
    20
    
    90
    
    Sample Output:
    
    220
    
    1800

---
+++ Football Stadium.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Football
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();//20
+        int b=sc.nextInt();//90
+        System.out.println((a+b)*2);
+        System.out.println(a*b);
+    }
+}
