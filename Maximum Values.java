6974e0ffee37892625f35939d766ab740a688947
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-12-01 14:31:01 +0530

    Create Maximum Values.java
    Write a program to find the maximum element in an array:
    
    Sample Input:
    5
    1
    2
    33
    4
    5
    
    Sample Output:
    33

---
+++ Maximum Values.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Max
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+            if(a[i]<a[j])
+            {
+                a[i]=a[i]+a[j];
+                a[j]=a[i]-a[j];
+                a[i]=a[i]-a[j];
+            }
+            }
+        }
+        System.out.println(a[0]);
+    }
+}
