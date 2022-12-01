ca67562156a7a2abfb80e4b7e94bc76473c4deef
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2022-12-01 10:45:56 +0530

    Create Sum of natural Numbers.java
    Write a program to find the sum of first N natural numbers.
    
    Input & Output Format:
    
    Input consists of one integer.
    
    Output consists of a integer.
    
    Sample Input & Output:
    
    5
    
    15

---
+++ Sum of natural Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m;
+         m=(n*(n+1))/2;
+        System.out.println(m);
+    }
+}
