e9a3e62cfdad2d50c2ad7462765e308303df8b5c
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-11 18:44:34 +0530

    Create difference.java

---
+++ difference.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=sc.nextInt();
+        int c=n-a;
+        if(n>0 && a>0)
+        {
+            System.out.println(c);
+        }
+        else
+        {
+            System.out.println(c);
+        }
+    }
+}
