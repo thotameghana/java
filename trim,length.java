dc1e0a819b02763a142bc825eac50a106300bc73
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-12 08:00:03 +0530

    Create trim,length.java

---
+++ trim,length.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String s2=sc.nextLine();
+        System.out.println(s.trim());
+        System.out.println(s2.trim());
+         System.out.println(s.length());
+        System.out.println(s2.length());
+        System.out.println(s2.toLowerCase());
+    }
+}
