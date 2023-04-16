e18fd6cc272773e65a563f41a6b7c909ae5e9059
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-16 18:48:25 +0530

    Create simple.java
    You are given a base, N, and a solution, Y. You must find the exponent needed to get from the base to the solution (such that N ^ X = Y) and print it.
    Input
    Line 1: An integer, N, for the base
    Line 2: An integer, Y, for the solution
    Output
    Line 1: An integer for the exponent needed to get from N to Y
    Constraints
    N > 0
    Y > 0
    Example
    Input
    2
    4
    Output
    2

---
+++ simple.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int Y = in.nextInt();
+        System.out.println(Y/N);
+    }
+}
