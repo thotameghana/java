6c855e772f41fb6275280ffc12174e4b201376aa
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-16 16:17:44 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        System.out.println(a*c+b+" "+c);
+    }
+}
