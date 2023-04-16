f9e77e3afca5c1f3d80843756be4129577d663f5
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-16 16:42:18 +0530

    Create trapazoidal.java
    In first line you have a means short base of the trapezoid.
    In second line you have b means long base of the trapezoid.
    In third line you have h means height.
    Output
    One decimal number, the area of this trapezoid
    Constraints
    0<a,b,c
    Example
    Input
    1
    2
    3
    Output
    4.5

---
+++ trapazoidal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int h = in.nextInt();
+        System.out.println(0.5*(a+b)*h);
+    }
+}
