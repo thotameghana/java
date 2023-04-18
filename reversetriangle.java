9dbe47f86cac51304bb5da9bccc25865a6b36f66
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-18 19:51:23 +0530

    Create reversetriangle.java

---
+++ reversetriangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i=1;i<=N;i++)
+        {
+            for(int j=i;j<=N;j++)
+            {
+                System.out.print(N);
+            }
+            System.out.println();
+        }
+    }
+}
