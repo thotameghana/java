c2d611ffb2ed92aa45603a0078a2d564a7086432
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-20 18:25:55 +0530

    Create nthnumber.java
    You have to find the n-th term of arithmetic progression.
    A is the first term of the arithmetic progression.
    D is the common difference of the arithmetic progression.
    'N' stands for the term number
    Input
    Line 1 A is a positive integer.
    Line 2 D is a positive integer.
    Line 3 N is a positive integer and stand for term number.
    Output
    Output the n-th term of arithmetic progression
    Constraints
    1<=A<=100000
    1<=D<=100000
    1<=N<=100000
    Example
    Input
    1
    2
    1
    Output
    1

---
+++ nthnumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int d = in.nextInt();
+        int n = in.nextInt();
+        System.out.println(a+(n-1)*d);
+    }
+}
