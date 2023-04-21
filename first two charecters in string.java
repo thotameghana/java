b7898717d59cb2340f94b025f4e2851edd5259f5
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-21 11:22:08 +0530

    Create first two charecters in string.java
    Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
    Input
    The main string str.
    Output
    3 copies of the first 2 chars of the original string
    Constraints
    Example
    Input
    hello
    Output
    hehehe

---
+++ first two charecters in string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        for(int i=1;i<=3;i++)
+        {
+             System.out.print(str.substring(0,2));
+        }
+    }
+}
