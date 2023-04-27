d3ef16a199258a68727238efd300f2ce1132a280
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-27 11:24:20 +0530

    Create square the first number and add 2nd number.java
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    1
    3 0
    9
    02 Test 2
    Input
    Expected output
    1
    3 1
    10
    03 Test 3
    Input
    Expected output
    3
    5 0
    5 2
    10 4
    25
    27
    104
    04 Test 4
    Input
    Expected output
    7
    48 3
    9 0
    30 44
    40 39
    17 1
    0 34
    6 22
    2307
    81
    944
    1639
    290
    34
    58

---
+++ square the first number and add 2nd number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            int A = in.nextInt();
+            int B = in.nextInt();
+             System.out.println(A*A+B);
+        }
+        //System.out.println(A*A+);
+    }
+}
