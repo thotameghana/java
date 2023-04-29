a5198507f64cf941c7fe63ccc204a06dc1e6d4db
Thota Meghana <116430940+Megharoyal@users.noreply.github.com>
2023-04-29 11:04:14 +0530

    Create square.java
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    6
    3600
    02 Test 2
    Input
    Expected output
    12
    14400
    03 Test 3
    Input
    Expected output
    20
    40000
    04 Test 4
    Input
    Expected output
    1
    100
    05 Test 5
    Input
    Expected output
    9
    8100

---
+++ square.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n*n*100);
+    }
+}
