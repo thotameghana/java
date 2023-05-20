147d51929c5872dbf6461b0bff1e1d1509cb67a2
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-20 12:07:06 +0530

    Create product of even elements.java

---
+++ product of even elements.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Delete
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int pro=1;
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.println("product of even elements");
+         for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+            pro=pro*a[i];
+            }
+        }
+            System.out.println(pro);
+    }
+}
