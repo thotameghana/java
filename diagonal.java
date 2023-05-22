c8ef38e3567f81ac892fb34203eb97bb2f39b4f0
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-22 12:23:49 +0530

    Create diagonal.java
    A diagonal of a polygon is defined as a segment joining any two non-consecutive vertices of the polygon.
    How many diagonals are there in a convex polygon with N vertices?
    Input
    N, the number of vertices
    Output
    D, the number of diagonals
    Constraints
    3 ≤ N ≤ 134217728
    Example
    Input
    4
    Output
    2

---
+++ diagonal.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        long N = in.nextLong();
+        long d=N*(N-3)/2;
+        System.out.println(d);
+    }
+}
