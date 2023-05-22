756ec56e1dc6758a0e09463e6a214e8d77915685
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-22 12:49:50 +0530

    Create chocolate bar of size.java
    I have a chocolate bar of size m*n. I wish to split it into unit-sized pieces. What is the minimum split I have to do with the bar?
     _ _      _   _      _   _   _   _
    |_|_| => |_| |_| => |_| |_| |_| |_|
    |_|_|    |_| |_|
    Example For a chocolate bar of size 2*2, after the first split, it becomes 2 bars of 1*2. It needs the second and third splits on each smaller bar to divide them into 4 units. Once a bar is separated, I cannot stack them up to split them together. I have to handle them separately.
    Minimum split is 3
    Input
    Line 1: Space-separated integers M and N, representing the width and height of the chocolate bar.
    Output
    The minimum number of splits necessary to divide the bar into 1*1 units
    Constraints
    1 ≤ M, N ≤ 100,000
    Example
    Input
    2 2
    Output
    3

---
+++ chocolate bar of size.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int M = in.nextInt();
+        int N = in.nextInt();
+        System.out.println((M*N)-1);
+    }}
