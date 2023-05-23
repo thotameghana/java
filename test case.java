5be8559b8e87db680ecf0dc196d44c1d4dba4619
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-23 08:21:30 +0530

    Create test case.java
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    8 2
    16106
    02 Test 2
    Input
    Expected output
    5 4
    2091
    03 Test 3
    Input
    Expected output
    9 6
    54153
    04 Test 4
    Input
    Expected output
    20 3
    602317

---
+++ test case.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int m=a*b;
+        int sum=a+b;
+        int d=a-b;
+    
+        System.out.print(m);
+        System.out.print(sum);
+        System.out.print(d);
+        
+    }
+}
