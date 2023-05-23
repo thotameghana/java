43ff1f57e9b154fec334636e6c6b2ad6b88a82ef
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-23 08:12:26 +0530

    Create plant as many trees.java
    John's task is to plant as many trees as he can in a square grid
    One tree takes up 2x2 spaces
    Input
    An integer n Length of one side of a square
    Output
    An integer a The most number of trees John can plant
    Constraints
    2 < n < 50
    Example
    Input
    2
    Output
    1

---
+++ plant as many trees.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m=n/2;
+        System.out.println(m*m);
+    }
+}
