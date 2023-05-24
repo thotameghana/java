661698cf45d05362d68d5ecbdcf6f85c03d9ee78
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-24 07:02:50 +0530

    Create sum of n odd numbers.java
    Your task is to find the sum of the first N odd natural numbers.
    Input
    Line 1: an integer N number of odds to sum up
    Output
    Line 1: sum of the first N odds
    Constraints
    0 < N < 1000
    Example
    Input
    1
    Output
    1

---
+++ sum of n odd numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        double sum=Math.pow(n,2);
+        System.out.println((int)sum);
+    }
+}
