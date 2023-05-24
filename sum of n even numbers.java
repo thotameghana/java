ebe6bc85e8f8858b5380336bc79ab147375f8b25
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-24 07:31:24 +0530

    Create sum of n even numbers.java
    You must ouput the sum of the first N even natural numbers.
    For example, if N=4 we get 2, 4, 6, 8 and the sum of these numbers is 20.
    Input
    Line 1: An integer N for the number of even natural numbers to sum.
    Output
    Line 1 : The sum.
    Constraints
    0 ≤ N ≤ 10^9
    Example
    Input
    4
    Output
    20

---
+++ sum of n even numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n*(n+1));
+    }
+}
