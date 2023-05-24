fe1b6ef8050bb0947bf50fe3ad540a607465d4b0
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-24 10:21:23 +0530

    Create Jack Goff.java
    Help Jack Goff find a seat in classroom which is at equal distance from his friend Bob and crush Erica such that he's as near to them as possible.
    It is guaranteed that a seat always exist in middle of Bob and Erica, as they are either sitting in the same row or same column or sitting diagonally to each other.
    Input
    The first line contains row and column number of Bob's seat: rb cb
    The second line contains row and column number of Erica's seat: re ce
    Output
    Output the row and column number of Jack's seat: rj cj
    Constraints
    1≤ rb, cb, re, ce ≤ 10
    Example
    Input
    1 3
    3 1
    Output
    2 2

---
+++ Jack Goff.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int rb = in.nextInt();
+        int cb = in.nextInt();
+        int re = in.nextInt();
+        int ce = in.nextInt();
+        int a=(rb+re)/2;
+        int b=(cb+ce)/2;
+        System.out.print(a+" ");
+        System.out.print(b);
+    }
+}
