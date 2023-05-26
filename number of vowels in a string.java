b2b55cfd495194c233a0b83c0ef525014d4e8da3
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-26 09:57:28 +0530

    Create number of vowels in a string.java
    You must output the number of vowels in a string.
    Y and y are not vowels!
    Input
    A single line: A string str.
    Output
    An integer: The number of vowels in the string.
    Constraints
    0 < string length <= 100
    Example
    Input
    Hello World!
    Output
    3

---
+++ number of vowels in a string.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        int count=0;
+        for(int i=0;i<str.length();i++)
+        {
+            char c=str.charAt(i);
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+            {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
