e73f87b33d5197b15950e85b50e1846c1c075833
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-28 19:54:43 +0530

    Create n pattern 15.java
    5
    12345
    1234
    123
    12
    1

---
+++ n pattern 15.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i,p=1;j<=n;j++,p++)
+            {
+                System.out.print(p);
+            }
+            System.out.println();
+        }
+    }
+}
