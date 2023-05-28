913bcacff99432f6d90d9abd74cda800df8019ef
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-28 19:50:50 +0530

    Create n pattern 13.java

---
+++ n pattern 13.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i,p=n;j<=n;j++,p--)
+            {
+                System.out.print(p);
+            }
+            System.out.println();
+            
+        }
+    }
+}
