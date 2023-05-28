046f5236bdc9d81b9315a8c4686d327dfefcd41a
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-05-28 19:48:18 +0530

    Create n pattern 12.java
    5
    54321
    5432
    543
    54
    5
    1
    21
    321
    4321
    54321

---
+++ n pattern 12.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i,p=n;j<=n;j++,p--)
+            {
+                System.out.print(p);
+            }
+            System.out.println();
+            
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1,p=i;j<=i;j++,p--)
+            {
+                System.out.print(p);
+            }
+            System.out.println();
+            
+        }
+    }
+}
